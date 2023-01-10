65d4f412b3a561f30bed4f613adc15adaa524917
Minzkraut <git@minzkraut.com>
2023-01-10 23:00:10 +0100

    Add route to get list of jobs

--- index.js
+++ index.js
@@ -18,6 +18,21 @@ app.get('/', (req, res) => {
   res.send('Job handling server')
 })
 
+app.get('/jobs', (req, res) => {
+  let queued = Object.values(jobs['queued']);
+  let waiting = Object.values(jobs['waiting']);
+  let finished = Object.values(jobs['finished']);
+
+  res.json( 
+    { 
+      'jobs': {
+        'queued': { 'count': queued.length, 'items': queued  },
+        'waiting': { 'count': waiting.length, 'items': waiting },
+        'finished': { 'count': finished.length, 'items': finished },
+      } 
+    }
+  );
+});
 
 app.listen(PORT, () => {
     console.log("Job Server running")
